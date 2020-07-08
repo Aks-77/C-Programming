60e7b1171a135bb4ae0f5c6312143f06e9ca45b4
Coursera Learner <nobody@nowhere.nul>
2020-07-08 15:16:43 +0000

    Did Assignment 02

--- 02_code1/code1.c
+++ 02_code1/code1.c
@@ -10,11 +10,11 @@ int max (int num1, int num2) {
 }
 
 int main(void) {
-  printf("max(42, -69) is %d\n", max(42, -69));
-  printf("max(33, 0) is %d\n", max(33, 0));
-  printf("max(0x123456, 123456) is %d\n", max(0x123456, 123456));
+  printf("max(42,-69) is %d\n", max(42, -69));
+  printf("max(33,0) is %d\n", max(33, 0));
+  printf("max(0x123456,123456) is %d\n", max(0x123456, 123456));
   //compute the max of 0x451215AF and 0x913591AF and print it out as a decimal number
-  printf("max(0x451215AF,0x913581AF) is %d\n",max(0x451215AF,0x913581AF));
+  printf("max(0x451215AF, 0x913581AF) is %d\n",max(0x451215AF, 0x913581AF));
   return 0;
 }
 
